6f7a94e978635e3f1befa87daac26c166dba3611
LuizPedroSousa <luizpedrosousa64@gmail.com>
2022-04-15 00:49:31 -0300

    refact(shared/domain): rename DomainException to BaseException :recycle:

--- src/infra/http/exceptions/CorsOriginException.ts
+++ src/infra/http/exceptions/CorsOriginException.ts
@@ -1,6 +1,6 @@
-import { DomainException } from '@shared/domain/DomainException';
+import { BaseException } from '@shared/domain/BaseException';
 
-export class CorsOriginException extends Error implements DomainException {
+export class CorsOriginException extends Error implements BaseException {
   constructor(origin: string) {
     super(`A política CORS para este site não permite acesso da Origem ${origin}.`);
 
