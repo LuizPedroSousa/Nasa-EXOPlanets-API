f319f2daab693f528e0c5cbc4f223040c824fc64
LuizPedroSousa <luizpedrosousa64@gmail.com>
2022-04-18 12:48:40 -0300

    feat(planets/domain): added PlanetHabitable ValueObject :sparkles:
    In PlanetHabitable ValueObject, added some validations to determine if
    the planet can be habitable.

---
+++ src/modules/planets/domains/PlanetHabitable.ts
@@ -0,0 +1,43 @@
+import { ValueObject } from '@shared/domain/ValueObject';
+
+interface PlanetHabitableProps {
+  value: boolean;
+}
+
+interface CreatePlanetHabitableDTO {
+  insolation: number;
+  disposition: string;
+  radius: number;
+}
+
+export class PlanetHabitable extends ValueObject<PlanetHabitableProps> {
+  get value(): boolean {
+    return this.props.value;
+  }
+
+  private constructor(props: PlanetHabitableProps) {
+    super(props);
+  }
+
+  public static create(data: CreatePlanetHabitableDTO): PlanetHabitable {
+    return new PlanetHabitable({ value: PlanetHabitable.isHabitable(data) });
+  }
+
+  public static isHabitable({ disposition, insolation, radius }: CreatePlanetHabitableDTO): boolean {
+    const dispositionRegex = /CONFIRMED/gi;
+
+    if (!dispositionRegex.test(disposition)) {
+      return false;
+    }
+
+    if (insolation < 0.37 || insolation > 1.11) {
+      return false;
+    }
+
+    if (radius < 0.5 || radius > 1.5) {
+      return false;
+    }
+
+    return true;
+  }
+}
