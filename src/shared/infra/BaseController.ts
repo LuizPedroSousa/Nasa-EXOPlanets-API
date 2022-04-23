d390894c633f3fdd8df977861a044820010f6d3f
LuizPedroSousa <luizpedrosousa64@gmail.com>
2022-04-23 15:59:28 -0300

    refact(shared/BaseController): change BaseController into a pure one :recycLe:

--- src/shared/infra/BaseController.ts
+++ src/shared/infra/BaseController.ts
@@ -1,4 +1,5 @@
-import { Request, Response } from 'express';
+import { HttpResponse } from './HttpResponse';
+import { HttpRequest } from './HttpRequest';
 
 interface JSONResponseDTO {
   status: number;
@@ -7,58 +8,63 @@ interface JSONResponseDTO {
 }
 
 export abstract class BaseController {
-  protected req: Request;
-  protected res: Response;
+  protected req: HttpRequest;
+  protected res: HttpResponse;
 
-  protected abstract handle(): Promise<void | any>;
+  protected abstract handle(request?: HttpRequest): Promise<HttpResponse | void>;
 
-  public async execute(req: Request, res: Response): Promise<void | any> {
+  public async execute(req: HttpRequest): Promise<void | any> {
     this.req = req;
-    this.res = res;
-    return this.handle();
+    return this.handle(this.req);
   }
 
-  public jsonResponse({ status, message, data }: JSONResponseDTO): Response {
-    return this.res.status(status).json({ message, data });
+  public jsonResponse({ message, status, data }: JSONResponseDTO): HttpResponse {
+    return {
+      status,
+      body: {
+        message,
+        ...data,
+      },
+    };
   }
 
-  public ok(data?: any): Response {
+  public ok(data?: any): HttpResponse {
     return this.jsonResponse({ message: 'OK', status: 200, data });
   }
 
-  public created(data?: any): Response {
+  public created(data?: any): HttpResponse {
     return this.jsonResponse({ message: 'Created', status: 201, data });
   }
 
-  public noContent(): Response {
+  public noContent(): HttpResponse {
     return this.jsonResponse({ status: 204 });
   }
 
-  public badRequest(message?: string): Response {
+  public badRequest(message?: string): HttpResponse {
     return this.jsonResponse({ message: message || 'Bad Request', status: 400 });
   }
 
-  public unauthorized(message?: string): Response {
+  public unauthorized(message?: string): HttpResponse {
     return this.jsonResponse({ message: message || 'Unauthorized', status: 401 });
   }
 
-  public forbidden(message?: string): Response {
+  public forbidden(message?: string): HttpResponse {
     return this.jsonResponse({ message: message || 'Forbidden', status: 403 });
   }
 
-  public notFound(message?: string): Response {
+  public notFound(message?: string): HttpResponse {
     return this.jsonResponse({ message: message || 'Not Found', status: 404 });
   }
 
-  public tooMany(message?: string): Response {
+  public tooMany(message?: string): HttpResponse {
     return this.jsonResponse({ message: message || 'Too Many Requests', status: 429 });
   }
 
-  public fail(message?: string): Response {
+  public fail(message?: string): HttpResponse {
     return this.jsonResponse({ message: message || 'Internal Server Error', status: 500 });
   }
 
-  public badGateway(message?: string): Response {
+  public badGateway(message?: string): HttpResponse {
     return this.jsonResponse({ message: message || 'Bad Gateway', status: 502 });
   }
 }
---
+++ src/shared/infra/HttpRequest.ts
@@ -0,0 +1,15 @@
+export interface File {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  destination: string;
+  filename: string;
+  path: string;
+  size: string;
+}
+
+export interface HttpRequest {
+  body?: any;
+  file?: File;
+}
