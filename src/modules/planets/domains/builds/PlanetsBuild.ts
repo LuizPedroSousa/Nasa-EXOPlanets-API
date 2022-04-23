318206fbdccaeef07701ef74fd0884fad0b9e027
LuizPedroSousa <luizpedrosousa64@gmail.com>
2022-04-23 17:12:18 -0300

    test(planets/infra): add integration tests for ImportKeplerPlanetsController :white_check_mark:

--- src/modules/planets/domains/builds/PlanetsBuild.ts
+++ src/modules/planets/domains/builds/PlanetsBuild.ts
@@ -1,5 +1,5 @@
-import { Stellar } from "@modules/stellars/Stellar";
-import { Planet, CreatePlanetDTO } from "../Planet";
+import { Stellar } from '@modules/stellars/Stellar';
+import { Planet, CreatePlanetDTO } from '../Planet';
 
 interface CreateCSVDTO {
   data: Partial<CreatePlanetDTO>[];
@@ -8,20 +8,20 @@ interface CreateCSVDTO {
 export class PlanetsBuild {
   static generate(data?: Partial<CreatePlanetDTO>) {
     const planet = {
-      name: data?.name || "Tatooine",
-      disposition: data?.disposition || "CONFIRMED",
+      name: data?.name || 'Tatooine',
+      disposition: data?.disposition || 'CONFIRMED',
       insolation: data?.insolation || 0.37,
       radius: data?.radius || 1.5,
       stellar:
         data?.stellar ||
         Stellar.create({
           radius: 10,
-          mass: "1",
-          age: "1",
-          effective_temperature: "1",
-          smet: "1",
+          mass: '1',
+          age: '1',
+          effective_temperature: '1',
+          smet: '1',
         }),
-      telescope: data?.telescope || "Kepler",
+      telescope: data?.telescope || 'kepler',
     };
 
     return planet;
@@ -40,23 +40,13 @@ export class PlanetsBuild {
   }
 
   static createCSV({ data }: CreateCSVDTO) {
-    const header = [
-      "kepler_name,koi_disposition,koi_insol,koi_prad,koi_srad,koi_steff,koi_sage,koi_smet,koi_smass",
-    ];
+    const header = ['kepler_name,koi_disposition,koi_insol,koi_prad,koi_srad,koi_steff,koi_sage,koi_smet,koi_smass'];
 
-    const rows = data.map(
-      ({
-        name,
-        disposition,
-        insolation,
-        radius,
-        stellar: { props: stellar },
-      }) => {
-        return `${name},${disposition},${insolation},${radius},${stellar.radius},${stellar.effective_temperature},${stellar.age},${stellar.smet},${stellar.mass}`;
-      }
-    );
+    const rows = data.map(({ name, disposition, insolation, radius, stellar: { props: stellar } }) => {
+      return `${name},${disposition},${insolation},${radius},${stellar.radius},${stellar.effective_temperature},${stellar.age},${stellar.smet},${stellar.mass}`;
+    });
 
-    const csv = header.concat(rows).join("\n");
+    const csv = header.concat(rows).join('\n');
 
     return csv;
   }
---
+++ src/modules/planets/infra/http/controlers/kepler/ImportKeplerPlanetsController.spec.ts
@@ -0,0 +1,103 @@
+import request from 'supertest';
+
+import { main } from '@infra/http/main';
+import { FileHelper } from '@infra/test/helpers/FileHelper';
+
+import { PlanetsBuild } from '@modules/planets/domains/builds/PlanetsBuild';
+
+beforeEach(async () => {
+  main.init();
+  await FileHelper.dropFolder();
+});
+
+describe('[INTEGRATION] - [Planets] - ImportKeplerPlanetsController', () => {
+  it('should give a status 201 when the planets are imported', async () => {
+    const planets = [PlanetsBuild.generate(), PlanetsBuild.generate({ name: 'Jupiter' })];
+    const csv = PlanetsBuild.createCSV({ data: planets });
+
+    const file_identifier = 'valid_kepler_planets.csv';
+
+    await FileHelper.writeFile(csv, file_identifier);
+
+    const { status, body } = await request(main.app)
+      .post('/planets/kepler/import')
+      .set('Accept', 'application/json')
+      .attach('planets', FileHelper.getFilePath(file_identifier));
+
+    expect(status).toBe(201);
+
+    expect(body?.planets).toHaveLength(2);
+    expect(body).toEqual({
+      message: 'Created',
+      planets: [
+        expect.objectContaining({
+          id: body?.planets[0].id,
+          ...planets[0],
+          stellar: {
+            ...planets[0].stellar.toJSON(),
+            id: body.planets[0].stellar.id,
+          },
+        }),
+        expect.objectContaining({
+          id: body?.planets[1].id,
+          ...planets[1],
+          stellar: {
+            ...planets[1].stellar.toJSON(),
+            id: body.planets[1].stellar.id,
+          },
+        }),
+      ],
+    });
+  });
+
+  it('should give a status 400, when planet disposition is invalid', async () => {
+    const planets = [PlanetsBuild.generate(), PlanetsBuild.generate({ disposition: 'invalid' })];
+    const csv = PlanetsBuild.createCSV({ data: planets });
+
+    const file_identifier = 'invalid_kepler_planets.csv';
+
+    await FileHelper.writeFile(csv, file_identifier);
+
+    const { status, body } = await request(main.app)
+      .post('/planets/kepler/import')
+      .set('Accept', 'application/json')
+      .attach('planets', FileHelper.getFilePath(file_identifier));
+
+    expect(status).toBe(400);
+
+    expect(body).toEqual({
+      message: 'invalid is not a valid planet disposition',
+    });
+  });
+
+  it('should give a status 400, when send a file with invalid extension', async () => {
+    const planets = [PlanetsBuild.generate(), PlanetsBuild.generate()];
+    const csv = PlanetsBuild.createCSV({ data: planets });
+
+    const file_identifier = 'invalid_kepler_planets.csvv';
+
+    await FileHelper.writeFile(csv, file_identifier);
+
+    const { status, body } = await request(main.app)
+      .post('/planets/kepler/import')
+      .set('Accept', 'application/json')
+      .attach('planets', FileHelper.getFilePath(file_identifier));
+
+    expect(status).toBe(400);
+    expect(body).toEqual({
+      message: 'Invalid file extension: bin',
+    });
+  });
+
+  it('should give a status 400 when send an empty file', async () => {
+    const { status, body } = await request(main.app).post('/planets/kepler/import').set('Accept', 'application/json');
+
+    expect(status).toBe(400);
+
+    expect(body).toEqual(
+      expect.objectContaining({
+        message: 'Missing params: planets',
+      }),
+    );
+  });
+});
