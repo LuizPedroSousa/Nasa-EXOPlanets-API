46e3bdcf08335406b1be27543bc9b9011f95c7d0
LuizPedroSousa <luizpedrosousa64@gmail.com>
2022-04-23 17:04:21 -0300

    fix(infra/ExceptionHandler): fix handlings for specific exceptions :bug:

--- src/infra/http/middlewares/ExceptionHandler.ts
+++ src/infra/http/middlewares/ExceptionHandler.ts
@@ -1,17 +1,43 @@
-import { Request, Response, NextFunction } from "express";
+import { CorsOriginException } from '@shared/infra/exceptions/CorsOriginException';
+import { InvalidFileExtensionException } from '@shared/infra/exceptions/InvalidFileExtensionException';
+import { InvalidFileSizeException } from '@shared/infra/exceptions/InvalidFileSizeException';
+import { Request, Response, NextFunction } from 'express';
 
 class ExceptionHandler {
   public handle(
-    exception: Error,
+    exception: InvalidFileSizeException | InvalidFileExtensionException | CorsOriginException | Error,
     request: Request,
     response: Response,
-    _: NextFunction
+    _: NextFunction,
   ): Response {
+    const exceptions: any = {
+      InvalidFileExtensionException: {
+        statusCode: 400,
+        message: exception.message,
+      },
+      InvalidFileSizeException: {
+        statusCode: 400,
+        message: exception.message,
+      },
+      CorsOriginException: {
+        statusCode: 400,
+        message: exception.message,
+      },
+    };
+
+    const exceptionResponse = exceptions[exception.name];
+
+    if (exceptionResponse) {
+      return response.status(exceptionResponse.statusCode).json({
+        message: exceptionResponse.message,
+      });
+    }
+
     console.error(exception);
 
     return response.status(500).json({
-      status: "error",
-      message: "Falha interna no servidor.",
+      status: 'error',
+      message: 'Falha interna no servidor.',
     });
   }
 }
