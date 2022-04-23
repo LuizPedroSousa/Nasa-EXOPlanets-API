9985a6837dc3fe8fa16b06da0d9d7d5672906a8e
LuizPedroSousa <luizpedrosousa64@gmail.com>
2022-04-23 16:22:07 -0300

    fix(Planets/domains): fixing set of name on InvalidPlanetDispositionException :bug:

--- src/modules/planets/domains/exceptions/InvalidPlanetDispositionException.ts
+++ src/modules/planets/domains/exceptions/InvalidPlanetDispositionException.ts
@@ -3,5 +3,6 @@ import { BaseException } from '@shared/domain/BaseException';
 export class InvalidPlanetDispositionException extends Error implements BaseException {
   constructor(disposition: string) {
     super(`${disposition} is not a valid planet disposition`);
+    this.name = 'InvalidPlanetDispositionException';
   }
 }
